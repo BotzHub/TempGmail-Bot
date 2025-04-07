35f61ebc33b2a96f73c49a7c8d227d1c955ed961
Mr Ghost <s.rytgam.ing89@gmail.com>
2025-04-07 21:53:33 +0530

    Update TEMPMAIL.py

--- TEMPMAIL.py
+++ TEMPMAIL.py
@@ -81,7 +81,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         keyboard = [
             [
                 InlineKeyboardButton("👨‍💻 Developer", url="https://t.me/SmartEdith_Bot"),
-                InlineKeyboardButton("📢 Channel", url="https://t.me/Tech_Shreyansh")
+                InlineKeyboardButton("📢 Channel", url="https://t.me/Tech_Shreyansh1")
             ]
         ]
         await update.message.reply_text(
